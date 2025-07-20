0e3eb2f66916c0c9fe011bfb0ffe62b28eb44c2b
Egor <egoryarovitsyn1@gmail.com>
2025-07-20 17:17:56 +0300

    feat add layout and header with main

--- src/pages/rootPage/ui/rootPage.tsx
+++ src/pages/rootPage/ui/rootPage.tsx
@@ -1,10 +1,23 @@
+import { Header } from "@/widgets";
 import { Suspense } from "react";
 import { Outlet } from "react-router-dom";
 
 const RootPage = () => {
   return (
     <Suspense>
-      <Outlet />
+      <div className="flex flex-col h-screen">
+        <Header />
+        <div className="flex flex-1 h-0  max-w-[1250px] mx-auto w-full">
+          <div className="flex flex-col flex-1 h-full">
+            <main className="flex-1 p-6 bg-white border overflow-y-auto">
+              <Outlet />
+            </main>
+            <footer className="bg-gray-800 text-white text-center py-4 border">
+              Footer
+            </footer>
+          </div>
+        </div>
+      </div>
     </Suspense>
   );
 };
---
+++ src/widgets/header/ui/header.tsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+  return (
+    <header className="sticky top-0 z-50 bg-white shadow-md h-20">
+      Header (sticky)
+    </header>
+  );
+};
